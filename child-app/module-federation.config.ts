b4d6d5a89b9dfad8c66e2b824356bc2c60f02afc
Eugene T <tobolovea@gmail.com>
2025-05-08 22:36:44 +0400

    init

---
+++ child-app/module-federation.config.ts
@@ -0,0 +1,5 @@
+export const mfConfig = {
+  name: "child_app",
+  exposes: {},
+  shared: ["vue"],
+};
---
+++ host-app/module-federation.config.ts
@@ -0,0 +1,5 @@
+export const mfConfig = {
+  name: "host_app",
+  exposes: {},
+  shared: ["react", "react-dom"],
+};
